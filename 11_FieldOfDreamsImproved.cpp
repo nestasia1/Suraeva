18d4a58e121a6b2e65460af35338dab515d8c55e
nestasia1 <109359981+nestasia1@users.noreply.github.com>
2022-10-03 09:23:01 +0300

    Update 11_FieldOfDreamsImproved.cpp

--- 11_FieldOfDreamsImproved.cpp
+++ 11_FieldOfDreamsImproved.cpp
@@ -9,20 +9,20 @@ using namespace std;
 
 int main()
 {
-    char word[100], wordSecret[10];               
-    bool help1 = false, help2 = false;
+    char word[100], wordSecret[10];            //массивы для исходного слова и массив для того, чтобы угадать введённое слово   
+    bool help1 = false, help2 = false;         // первая переменная показывает, угадали ли слово, вторая - угадана ли буква
+    char letter[100], letter1[100];            //массив для введённых букв (или введённого полностью слова) и вспомогательный массив для проверки, была ли введена данная буква раньше
+    
     cout << "Enter word: ";
     cin >> word;
     
-    for (int i = 0; i < strlen(word); i++) 
+    for (int i = 0; i < strlen(word); i++)    //заполняем второй массив символами "-" в количестве, равном буквам в введённом слове
         wordSecret[i] = '-';
-    char letter[100], letter1[100];
-    int j = 0;
 
     cout << "Enter letter: ";
     cin >> letter;
 
-    if (strlen(letter) != 1)
+    if (strlen(letter) != 1)                  //если введено больше одной буквы, начинается прверка введённого слова с исходным
     {
         for (int i = 0; i < strlen(letter); i++)    
         {                                   
@@ -35,23 +35,24 @@ int main()
         cout << "You are amazing!\n";
         return 0;
     }
-
+    
+    int j = 0;
     letter1[j] = letter[0];
     j++;
 
-    for (int i = 0; i < strlen(word); i++)    
+    for (int i = 0; i < strlen(word); i++)    //проверяем, угадали ли букву, и если да, то заменяем "-" на эту букву
         if (word[i] == letter[0])
         {
             wordSecret[i] = letter[0];                        
             help2 = true;                       
         }
 
-    if (help2 == false)  
+    if (help2 == false)                     //если не угадали букву
     {
         cout << "There is not this letter\n";
     }
 
-    if (help2 == true)                      
+    if (help2 == true)                      //если угадали букву, выводится второй массив с заменённым "-" на букву
     {
         for (int i = 0; i < strlen(word); i++)
         {
@@ -60,7 +61,7 @@ int main()
         cout << "\n";
     }
 
-    for (int i = 0; i < strlen(word); i++)    
+    for (int i = 0; i < strlen(word); i++)    //проверяем, угадано ли слово
     {                                   
         if (wordSecret[i] == '-')
         {
@@ -78,13 +79,13 @@ int main()
     }
 
 
-    while (help1 == false)                 
+    while (help1 == false)                 //пока слово не угадано
     {
         help2 = false;
         cout << "Enter letter: ";
         cin >> letter;
 
-        if (strlen(letter) != 1)
+        if (strlen(letter) != 1)           // если введено больше одной буквы, проверяем, совпадает ли введённое слово с исходным
         {
             for (int i = 0; i < strlen(letter); i++)  
             {                                  
@@ -98,7 +99,7 @@ int main()
             return 0;
         }
 
-        for (int k = 0; k < 100; k++)
+        for (int k = 0; k < 100; k++)      //проверяем, была ли введена данная буква раньше
         {
             if (letter1[k] == letter[0])
             {
@@ -110,20 +111,20 @@ int main()
         letter1[j] = letter[0];
         j++;
 
-        for (int i = 0; i < strlen(word); i++)     
+        for (int i = 0; i < strlen(word); i++)     //проверяем, угадали ли букву, и если да, то заменяем "-" на эту букву
             if (word[i] == letter[0])
             {
                 wordSecret[i] = letter[0];                         
                 help2 = true;                       
             }
         
-        if (help2 == false)  //сообщаем о том, что такой буквы нет (если её нет)
+        if (help2 == false)                  //сообщаем о том, что такой буквы нет (если её нет)
         {
             cout << "There is not this letter\n";
         }
                 
 
-        if (help2 == true)                   
+        if (help2 == true)                   //если угадали букву, выводится второй массив с заменённым "-" на букву
         {
             for (int i = 0; i < strlen(word); i++)
             {
@@ -133,7 +134,7 @@ int main()
         }
 
 
-        for (int i = 0; i < strlen(word); i++)    
+        for (int i = 0; i < strlen(word); i++)    //проверем, угадано ли слово
         {                               
             if (wordSecret[i] == '-')
             {
