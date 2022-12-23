94ae5f6def44dd80fc0b7939071d7a7522fb1636
nestasia1 <109359981+nestasia1@users.noreply.github.com>
2022-12-23 15:26:35 +0300

    Update CStructure.h

--- LIFO & FIFO better/CStructure.h
+++ LIFO & FIFO better/CStructure.h
@@ -12,7 +12,13 @@ class CStructure
 		vector<T> arr;
 
 	public:
-		void print(void);
+		void print(void)
+		{
+			for (int i = 0; i < this->arr.size(); i++)
+			{
+				cout << this->arr[i] << " ";
+			}
+		}
 		virtual void push(T n) = 0;
 		virtual T pop() = 0;
 };
